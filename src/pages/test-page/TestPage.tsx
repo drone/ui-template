ee681443a60028150bda459da13aee821bf648d1
Tan Nhu <tan@harness.io>
2021-09-20 11:02:21 -0700

    Update TestPage

--- src/pages/test-page/TestPage.tsx
+++ src/pages/test-page/TestPage.tsx
@@ -4,9 +4,10 @@ import css from './TestPage.module.scss'
 
 export const TestPage: React.FC = () => {
   const { getString } = useStrings()
+
   return (
     <>
-      <h1>Hello World 1 {getString('harness')}</h1>
+      <h1>Hello {getString('harness')}</h1>
       <div className={css.container}>This is a test page</div>
     </>
   )
