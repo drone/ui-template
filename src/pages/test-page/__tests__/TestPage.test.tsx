2bb70a95dbc04fffbdabe35e5dd78375d576e74d
Tan Nhu <tan@harness.io>
2021-09-20 10:59:35 -0700

    Add utilities for Jest testing

--- src/pages/test-page/__tests__/TestPage.test.tsx
+++ src/pages/test-page/__tests__/TestPage.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { TestWrapper } from 'utils/testUtils'
 import { TestPage } from '../TestPage'
 
 describe('Test TestPage', () => {
   test('Test rendering', () => {
-    const { container } = render(<TestPage />)
+    const { container } = render(
+      <TestWrapper>
+        <TestPage />
+      </TestWrapper>
+    )
     expect(container).toMatchSnapshot()
   })
 })
